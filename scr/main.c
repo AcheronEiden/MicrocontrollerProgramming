371adc0656323582059e8a34d396e17c1fc369ef
AcheronEiden <76567363+AcheronEiden@users.noreply.github.com>
2023-01-25 13:48:56 +0100

    Add files via upload

--- scr/main.c
+++ scr/main.c
@@ -1,52 +1,64 @@
 /*
- * 20210907_m328_st7735_lcd.c
- *
- * Created: 2021-09-07 20:25:40
- * Author : uwezi
+ Created: 2023-01-23
+ Author : Alhassan Jawad & Andreas Hertzberg (Group ANIMATION)
  */ 
 
-#include <avr/io.h> // includes the apropriate IO definition usage
-#include <util/delay.h> // includes the apropriate delay functions
-#include <math.h>
+//--------------------------Include(s)------------------------------------------------------------------
+#include <stdio.h> // Allows us to perform input and output operations
+#include <avr/io.h> // Includes the apropriate IO definition usage
+#include <util/delay.h> // Includes the apropriate delay functions
+#include <math.h> // Set of functions to compute common mathematical operations and transformations
+#include <avr/interrupt.h> // Include the interrupt library
 #include "spi.h"
 #include "st7735.h"
 #include "st7735_gfx.h"
 #include "st7735_font.h"
-
 #include "logo_bw.h"
 #include "free_sans2.h"
+//------------------------------------------------------------------------------------------------------
 
-#include <stdio.h> // allows us to perform input and output operations
+char buffer[40]; // Buffer denotes a computer memory block that acts as a temporary placeholder
+volatile int counter = 0; // Decleared as volatile so microcontroller knows it can be changed
 
-
-char buffer[40];
-
-int main(void) 
+// Using External Interrupt Request 0 that will activate when the pushbutton is pressed
+ISR(INT0_vect)
 {
-  int counter = 0;
+  // RESET ANIMATION
+  st7735_fill_rect(0, 0, 128, 128, ST7735_COLOR_WHITE);
+  counter = 0; // Reset the animation sequence
+}
+
+int main(void)
+{
+  DDRD = 0xF0; // All D pins becomes outputs
+  PORTD |= 0xFC;
+  EICRA = 0x0a; // Configures so that when the button is pressed it becomes an input
+  sei(); // Enable global interrupts
+  EIMSK |= 0x03 ; // External Interrupt 0 (INT0) enabled
   
   spi_init();
   st7735_init();
 
   st7735_set_orientation(ST7735_PORTRAIT);  
-  st7735_fill_rect(0, 0, 128, 128, ST7735_COLOR_BLACK);
-  st7735_draw_fast_hline(10, 30, 100, ST7735_COLOR_RED);
+  st7735_fill_rect(0, 0, 128, 128, ST7735_COLOR_BLACK); // Covers the whole screen in black as bg color
 
   ADMUX = (0 << REFS1) | (1 << REFS0) | (0 << MUX3) | (0 << MUX2) | (0 << MUX1) | (0 << MUX0);
   ADCSRA = (1 << ADEN) | (1 << ADSC) | (1 << ADPS2) | (1 << ADPS1) | (0 << ADPS0);
 
   while (ADCSRA & (1 << ADSC));
-
+  
   while(1)
-  { 
-    st7735_fill_rect(0, 0, 128, 128, ST7735_COLOR_BLACK);
+  {
+    st7735_fill_rect(0, 0, 128, 128, ST7735_COLOR_BLACK); // Covers the whole screen in black as bg color
     if(counter % 10 == 0)
     {
-      // Display the red circle animation
+      // ANIMATION 1
+      // Display the red circle animation with radius 30 pixels centered at positions (64,64)
       st7735_draw_circle(64, 64, 30, ST7735_COLOR_RED);
-    }else
+    } else
     {
-      // Display the green rectangle animation
+      // ANIMATION 2
+      // Display the green rectangle animation with height/width of 80 pixels centered at (20, 20)
       st7735_draw_rect(20, 20, 80, 80, ST7735_COLOR_GREEN);
     }
     counter += 5;
